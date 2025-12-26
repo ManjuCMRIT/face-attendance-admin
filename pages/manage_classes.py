0e0be17237c029d71ad58cfcdac468cc2ff2ab96
ManjuCMRIT <121920349+ManjuCMRIT@users.noreply.github.com>
2025-12-26 23:27:30 +0530

    Update manage_classes.py

--- pages/manage_classes.py
+++ pages/manage_classes.py
@@ -1,1 +1,17 @@
+import streamlit as st
+from utils.firebase_utils import db
+
+def show():
+    st.title("📂 Manage Classes")
+
+    classes = db.collection("classes").stream()
+
+    for cls in classes:
+        data = cls.to_dict()
+        label = f"{data['department']} - {data['batch']} - {data['section']}"
+
+        if st.button(label):
+            st.session_state["current_class"] = cls.id
+            st.session_state["page"] = "class_details"
+            st.rerun()
 
